25037f6617145e3eb0fbc9e4aa9e6b883dfd12e7
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-03-01 00:31:09 +0600

    next js added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -47,6 +48,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button variant={"outline"}> Okay Now </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
