5b09ffaf8fda98a233ef352fdc529163e7c12292
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-03-03 07:07:09 +0600

    admin dashboard create using shadcn

---
+++ client/src/components/dashboard/sidebar/app-sidebar.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import * as React from "react";
+import {
+  Bot,
+  Frame,
+  LifeBuoy,
+  Map,
+  PieChart,
+  Send,
+  SquareTerminal,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { NavMain } from "./nav-main";
+import { NavUser } from "./app-user";
+
+const data = {
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/admin",
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: "Products",
+      url: "/admin/products",
+      icon: Bot,
+      items: [
+        {
+          title: "Add Products",
+          url: "/admin/create-product",
+        },
+        {
+          title: "All Products",
+          url: "/admin/proudcts",
+        },
+      ],
+    },
+    {
+      title: "Categories",
+      url: "/admin/categories",
+      icon: Bot,
+      items: [
+        {
+          title: "Manage Categories",
+          url: "/admin/category",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Support",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: Send,
+    },
+  ],
+  projects: [
+    {
+      name: "Design Engineering",
+      url: "#",
+      icon: Frame,
+    },
+    {
+      name: "Sales & Marketing",
+      url: "#",
+      icon: PieChart,
+    },
+    {
+      name: "Travel",
+      url: "#",
+      icon: Map,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/">
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <h2 className="font-bold text-xl">MediMart</h2>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
