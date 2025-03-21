e80d9449f5388e7faa4d60a41811aa2702971bde
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-03-21 21:46:10 +0600

    updated

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -18,7 +18,7 @@ export const middleware = async (request: NextRequest) => {
         if (authRoutes.includes(pathname)) {
             return NextResponse.next()
         } else {
-            return NextResponse.redirect(new URL(`http://localhost:3000/login?redirectPath=${pathname}`, request.url))
+            return NextResponse.redirect(new URL(`/login?redirectPath=${pathname}`, request.url))
         }
     }
 
